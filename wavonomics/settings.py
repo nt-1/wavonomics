fc72b2476f37d3be6e22651c24816a707d3f3912
antoinef <antoinef@Antoine-Frange-MacBook-Pro-2.local>
2017-07-10 22:33:45 +0100

    reset project

--- wavonomics/settings.py
+++ wavonomics/settings.py
@@ -190,38 +190,24 @@ PROJECT_ROOT = BASE_DIR = os.path.dirname(PROJECT_APP_PATH)
 # project specific.
 CACHE_MIDDLEWARE_KEY_PREFIX = PROJECT_APP
 
+# URL prefix for static files.
+# Example: "http://media.lawrence.com/static/"
+STATIC_URL = "/static/"
 
+# Absolute path to the directory static files should be collected to.
+# Don't put anything in this directory yourself; store your static files
+# in apps' "static/" subdirectories and in STATICFILES_DIRS.
+# Example: "/home/media/media.lawrence.com/static/"
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
 
-# # URL prefix for static files.
-# # Example: "http://media.lawrence.com/static/"
-# STATIC_URL = '/static/'
-#
-# # Absolute path to the directory static files should be collected to.
-# # Don't put anything in this directory yourself; store your static files
-# # in apps' "static/" subdirectories and in STATICFILES_DIRS.
-# # Example: "/home/media/media.lawrence.com/static/"
-# STATIC_ROOT = '/www/static'
+# URL that handles the media served from MEDIA_ROOT. Make sure to use a
+# trailing slash.
+# Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
+MEDIA_URL = STATIC_URL + "media/"
 
-# Static files borrowed from image of the day
-
-STATIC_ROOT = ''
-STATIC_URL = '/static/'
-STATICFILES_DIRS = (os.path.join(BASE_DIR, 'static'),)
-
-#MEDIA FILE (user uploaded files)
-MEDIA_ROOT = os.path.join(BASE_DIR, "..", "www", "media")
-MEDIA_URL = '/media/'
-
-# end of image of the day copy paste
-
-# # URL that handles the media served from MEDIA_ROOT. Make sure to use a
-# # trailing slash.
-# # Examples: "http://media.lawrence.com/media/", "http://example.com/media/"
-# MEDIA_URL = STATIC_URL + "media/"
-#
-# # Absolute filesystem path to the directory that will hold user-uploaded files.
-# # Example: "/home/media/media.lawrence.com/media/"
-# MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
+# Absolute filesystem path to the directory that will hold user-uploaded files.
+# Example: "/home/media/media.lawrence.com/media/"
+MEDIA_ROOT = os.path.join(PROJECT_ROOT, *MEDIA_URL.strip("/").split("/"))
 
 # Package/module name to import the root urlpatterns from for the project.
 ROOT_URLCONF = "%s.urls" % PROJECT_APP
