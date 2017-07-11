ea169ebc713c6516f86dbbd4b20c8bc0d1fccfc8
antoinef <antoinef@Antoine-Frange-MacBook-Pro-2.local>
2017-07-11 12:32:56 +0100

    changed STATIC ROOT and added random file in www/static for git

--- wavonomics/settings.py
+++ wavonomics/settings.py
@@ -198,7 +198,7 @@ STATIC_URL = "/static/"
 # Don't put anything in this directory yourself; store your static files
 # in apps' "static/" subdirectories and in STATICFILES_DIRS.
 # Example: "/home/media/media.lawrence.com/static/"
-STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/"))
+STATIC_ROOT = os.path.join(PROJECT_ROOT, STATIC_URL.strip("/www/static"))
 
 # Extra places for collectstatic to find static files.
 STATICFILES_DIRS = (
